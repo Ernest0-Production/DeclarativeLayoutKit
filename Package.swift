bd23566a195ca06a00153a2532248c2f8fca079a
ernest0n <magatar136@yandex.ru>
2021-06-05 15:42:13 +0300

    Remove Styling Module

--- Package.swift
+++ Package.swift
@@ -7,10 +7,9 @@ let package = Package(
     name: "DeclarativeLayoutKit",
     platforms: [.iOS(.v10)],
     products: [
-        .library(name: "DeclarativeLayoutKit", targets: ["Layouting", "Chaining", "Styling"]),
+        .library(name: "DeclarativeLayoutKit", targets: ["Layouting", "Chaining"]),
         .library(name: "DeclarativeLayoutKit/Layouting", targets: ["Layouting"]),
         .library(name: "DeclarativeLayoutKit/Chaining", targets: ["Chaining"]),
-        .library(name: "DeclarativeLayoutKit/Styling", targets: ["Styling"]),
     ],
     dependencies: [
         .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.0"),
@@ -18,7 +17,6 @@ let package = Package(
     targets: [
         .target(name: "Layouting", dependencies: ["SnapKit"], path: "Sources/DeclarativeLayoutKit/Layouting"),
         .target(name: "Chaining", path: "Sources/DeclarativeLayoutKit/Chaining"),
-        .target(name: "Styling", path: "Sources/DeclarativeLayoutKit/Styling"),
 
         .testTarget(name: "LayoutingTests", dependencies: ["Layouting"]),
     ],
